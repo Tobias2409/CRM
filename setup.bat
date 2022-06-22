6658fcb044fd8a00b7fd04c7ae2ed7e320369a0f
Tobias2409 <60622539+Tobias2409@users.noreply.github.com>
2022-06-22 21:49:34 +0200

    Add PIP installs

--- setup.bat
+++ setup.bat
@@ -1,2 +1,11 @@
+pip install opencv-python
+pip install face_recognition
+pip install mtcnn
+pip install tensorflow
+pip install keras
+pip install keras_vggface
+pip install keras_applications
+pip install icrawler
+
 mkdir download
 mkdir img
